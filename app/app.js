3539c99207320130c3e79759608b12c6611489f0
conghoacntt13t1 <conghoacntt13t1@gmail.com>
2017-11-30 23:10:33 +0700

    processing spot click event

--- app/app.js
+++ app/app.js
@@ -32,31 +32,3 @@ app.config(function ($routeProvider) {
             })
         .otherwise({ redirectTo: '/login' });
 });
-    // .factory('socket', function($rootScope) {
-    //
-    //     var socket = io.connect("localhost:16969");
-    //
-    //     return {
-    //         on: function(eventName, callback) {
-    //             socket.on(eventName, function() {
-    //                 var args = arguments;
-    //                 //console.log(args);
-    //                 $rootScope.$apply(function() {
-    //
-    //
-    //                     callback.apply(socket, args);
-    //                 });
-    //             });
-    //         },
-    //         emit: function(eventName, data, callback) {
-    //             socket.emit(eventName, data, function() {
-    //                 var args = arguments;
-    //                 $rootScope.$apply(function() {
-    //                     if (callback) {
-    //                         callback.apply(socket, args);
-    //                     }
-    //                 });
-    //             });
-    //         }
-    //     };
-    // });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app=express();
 var http=require('http').createServer(app);
-var io=require('socket.io')(http);
+// var io=require('socket.io')(http);
 
 app.use(express.static(__dirname + ''));
 
